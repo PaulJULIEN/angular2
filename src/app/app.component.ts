b96c5b263e0448be75b427a3502e4280fa117a9a
Aurélien Boncenne <aurelien.boncenne@gmail.com>
2017-01-29 23:03:04 +0100

    JESUS'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component,OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { json } from './json';
 import { JsonService } from './app.service';
 
